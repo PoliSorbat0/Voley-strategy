628d254114b16b3bb129c844d112ca1417120db4
Rodrigo <rodrigoneiraosses@gmail.com>
2026-04-15 12:34:21 -0400

    Springboot agregado

---
+++ Voley_Strategy/src/main/java/com/duoc/Voley_Strategy/VoleyStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.Voley_Strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoleyStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoleyStrategyApplication.class, args);
+	}
+
+}
---
+++ Voley_Strategy/src/test/java/com/duoc/Voley_Strategy/VoleyStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.Voley_Strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoleyStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
