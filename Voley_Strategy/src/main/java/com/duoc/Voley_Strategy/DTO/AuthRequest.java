573fb449b08c6c8c13f801065c5a57af5e4a8549
Rodrigo <Rodrigoneiraosses@gmail.com>
2026-06-19 20:50:49 -0400

    Security corregido + Postman endpoint usuario agregado

---
+++ Voley_Strategy/src/main/java/com/duoc/Voley_Strategy/DTO/AuthRequest.java
@@ -0,0 +1,9 @@
+package com.duoc.Voley_Strategy.DTO;
+
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    private String username;
+    private String password;
+}
---
+++ Voley_Strategy/src/main/java/com/duoc/Voley_Strategy/DTO/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.duoc.Voley_Strategy.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
